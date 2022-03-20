9b580e7d85174e7f07bae9e8dc4df997e31564ed
s3845230 <s3845230@student.rmit.edu.au>
2022-03-20 17:06:35 +1100

    initialised spring project

---
+++ movies/src/test/java/net/romn/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.romn.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
